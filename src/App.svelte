e833ec134826cb9bd209158d3aee84190f92fd3d
6rak0 <ego.6rak0@gmail.com>
2023-03-12 12:25:45 -0600

    feat: :sparkles: implemented qr creation

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,54 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import qrcode from "qrcode";
+  import PocketBase from "pocketbase";
+
+  let client;
+  let data;
+  let img;
+
+  const pb = new PocketBase(import.meta.env.VITE_DB_URL);
+
+  const addPackage = async () => {
+    const payload = {
+      client,
+      data,
+    };
+    const record = await pb.collection("packages").create(payload);
+    //console.log(record.id)
+    return record;
+  };
+
+  const getImage = async () => {
+    const pack = await addPackage();
+    console.log(pack);
+    const wawa = {
+      id: pack.id,
+      client: pack.client,
+      data: pack.data,
+    };
+    await qrcode.toDataURL(JSON.stringify(wawa), function (err, url) {
+      img = url;
+    });
+  };
+
+  function printImage() {
+    var printWindow = window.open("", "_blank", "height=400,width=600");
+    printWindow.document.write(
+      '<html><head><title>imprimir qr</title></head><body><img src="' +
+        img +
+        '"></body></html>'
+    );
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<input type="text" placeholder="client" bind:value={client} />
+<input type="text" placeholder="data" bind:value={data} />
+<button on:click={addPackage}>add package</button>
+<button on:click={getImage}>get image</button>
+<button on:click={printImage}>print</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<img src={img} alt="test" style="height: 300px; width:300px" />
